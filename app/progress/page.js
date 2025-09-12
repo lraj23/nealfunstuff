7ebd7382174196e08c41e742bfb57753c831dfa8
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2025-09-11 20:40:33 -0700

    create the section for links to other pages; steal ALL of neal.fun's images 😅 (i'll make my own...); start the progress page

---
+++ app/progress/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+	return (
+		<>
+			<h1 id="title">Progress</h1>
+			<p id="subtitle">Count your days! (ummm... I mean measure how much time is left 😅)<br/>a tiny variation of <a href="https://neal.fun/progress/">neal.fun/progress/</a></p>
+			<div id="pages">
+			</div>
+		</>
+	)
+}
---
+++ components/pageLinks.js
@@ -0,0 +1,9 @@
+function pageLink({ link, image }) {
+	return (
+		<div className="page">
+			<a href={link}><img src={image} /></a>
+		</div>
+	)
+}
+
+export default pageLink;
