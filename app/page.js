5232fb3c959226f1fe26b02078c43433ae1c1f17
Lavith <189830163+lraj23@users.noreply.github.com>
2025-09-15 14:34:52 -0700

    a test commit; please ignore this I'll undo do this VERY soon

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export const metadata = { title: "NealFunStuff" };
 export default _ => <>
 	<h1 id="title">NEAL.FUN Stuff</h1>
 	<p id="subtitle">a tiny variation of <a href="https://neal.fun/">neal.fun</a><br />All these stolen images ARE credited to neal.fun! I'll make my own eventually.</p>
+	<p>This is a test hopefully</p>
 	<div id="pages">
 		<PageLink link="./progress" image="./progress.png" />
 		<PageLink link="./speed" image="./speed.png" />
